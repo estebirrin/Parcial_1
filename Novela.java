f9d527ffb96f23d2b9a11ccb5dc9c4cbcf27e793
Esteban <estebanospinaluna@gmail.com>
2023-09-14 20:33:08 -0500

    tercer guardado

--- Novela.java
+++ Novela.java
@@ -2,9 +2,9 @@ public class Novela extends Libro {
     
     protected String Tipos;
 
-    public Novela(String Titulo, String Autor, int Precio, String Tipos) {
+    public Novela(String Titulo, String Autor, int Precio, String tipos) {
         super(Titulo, Autor, Precio);
-        Tipos = Tipos;
+        Tipos = tipos;
     }
 
     public void imprimirLibro(){
